ea7a4af4e5a5086b8e8fd011c0dab34d576429d6
arton <arton@765e6ed1-4a9f-4a2e-8956-0abba9a8a531>
2011-02-09 09:55:34 +0000

    remove OpenSSL Specific install process.

--- bat/uninstall.rb
+++ bat/uninstall.rb
@@ -1,10 +1,14 @@
 #!/usr/local/bin/ruby -Ks
 # encoding: cp932
 
+require 'winpath'
 require 'fileutils'
 
 $path = Pathname.new(ARGV[0]).shortname
 $altpath = $path.gsub(File::ALT_SEPARATOR, File::SEPARATOR)
-FileUtils.rm "#{$altpath}/ssleay32.dll"
-FileUtils.rm "#{$altpath}/libeay32.dll"
-FileUtils.rm "#{$altpath}/../lib/ruby/1.9.1/i386-mswin32_100/openssl.so"
+FileUtils.rm_f "#{$altpath}/ssleay32.dll"
+FileUtils.rm_f "#{$altpath}/libeay32.dll"
+FileUtils.rm_f "#{$altpath}/../OpenSSL-License.txt"
+FileUtils.rm_f "#{$altpath}/../lib/ruby/1.9.1/i386-mswin32_100/openssl.so"
+
+exit 0
