a57653c4a10673beca9b2f03ac4832921a4dc990
arton <arton@765e6ed1-4a9f-4a2e-8956-0abba9a8a531>
2011-05-03 03:17:55 +0000

    initial release for NougakuDo

---
+++ bat/post-install-1.9-64.rb
@@ -0,0 +1,78 @@
+#!/usr/local/bin/ruby -Ks
+# encoding: cp932
+require 'winpath'
+
+$failed = false
+begin
+  $log = File.open("#{ENV['TMP']}\\ruby-1.9-64-install.log", 'w')
+rescue
+  puts $!
+  $log = $stderr
+end
+if ARGV.size == 0
+  $log.puts 'no install directory, bye'
+  $log.close
+  gets
+  exit(1)
+end
+$path = Pathname.new(ARGV[0]).shortname
+$log.puts "path='#{$path}'"
+$altpath = $path.gsub(File::ALT_SEPARATOR, File::SEPARATOR)
+$log.puts "altpath='#{$altpath}'"
+def reset_path(pn)
+  s = "@echo off\r\n"
+  begin
+    start = false
+    File.open("#{ARGV[0]}\\#{pn}", 'r').each_line do |line|
+      unless start
+        next unless /\A:WinNT/ =~ line
+        start = true
+      else
+        s << line + "\r\n"
+      end
+    end.close
+    if !start
+      $log.puts "skip -- #{pn}"
+      return
+    end
+    s.gsub! /^\\usr\\bin\\ruby/, "#{$path}\\ruby"
+    s.gsub! %r|/usr/bin/|, "#{$altpath}/"
+    File.open("#{ARGV[0]}\\#{pn}", 'w') do |dst|
+      dst.write s
+    end
+  rescue
+    $log.puts "#{$!}"
+    $failed = true
+  end
+  $log.puts "#{pn} done"
+end
+
+def reset_shbang(pn)
+  s = nil
+  begin
+    File.open("#{ARGV[0]}\\#{pn}", 'r') do |f|
+      s = f.read
+    end
+    unless s =~ /\A#!.+ruby.+$/
+      $log.puts "skip -- #{pn}"
+      return
+    end  
+    s.gsub! /\A#!.+ruby.+$/, "#!#{$altpath}/ruby.exe"
+    File.open("#{ARGV[0]}\\#{pn}", 'w') do |dst|
+      dst.write s
+    end
+  rescue
+    $log.puts "#{$!}"
+    $failed = true
+  end
+  $log.puts "#{pn} done"
+end
+
+Dir.open(ARGV[0]).each do |nm|
+  reset_path(nm) if /\.(cmd|bat)\Z/i =~ nm
+  reset_shbang(nm) if /\A[^.]+\Z/ =~ nm
+end
+
+$log.puts "exit=#{$failed}"
+$log.close
+exit ($failed) ? 1 : 0
